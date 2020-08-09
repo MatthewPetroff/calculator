2c697490c45e40386e6874de699ded2ced69023d
Matt <matthewjpetroff@gmail.com>
2020-08-09 14:33:24 -0400

    Complete operations buttons

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let memoryRecall = null;
+let operation = null;
 let memory = null;
+let calculatedNum = null;
+let displayNum = '0';
 
-function getDisplayNumber() {
-	return document.getElementById('display').innerHTML;
+
+function display(string) {
+	document.getElementById('display').textContent = string;
 }
 
-function notZero(){
-	let displayNum = document.getElementById('display').innerHTML;
+function updateDisplayNum(char) {
+	if (displayNum === '0') {
+		if (char === '.') {
+			displayNum = '0.';
+		} else {
+			displayNum = char;	
+		}
+	} else {
+		displayNum += char;
+	}
+}
+
+function notZero() {
 	if (displayNum != '0') {
 		return true;
 	} else {
@@ -15,7 +29,6 @@ function notZero(){
 }
 
 function noDecimal() {
-	let displayNum = document.getElementById('display').innerHTML;
 	for (let i = 0; i < displayNum.length; i++) {
 		if (displayNum[i] === '.') {
 			return false;
@@ -24,104 +37,156 @@ function noDecimal() {
 	return true;
 }
 
-function appendDigits(num) {
-	if (getDisplayNumber() === '0') {
-		document.getElementById('display').innerHTML = num;	
+function calculate() {
+	switch (operation) {
+		case 'add':
+			return toString(Number(calculatedNum) + Number(displayNum));
+		case 'subtract':
+			return toString(Number(calculatedNum) - Number(displayNum));
+		case 'multiply':
+			return toString(Number(calculatedNum) * Number(displayNum));
+		case 'divide':
+			return toString(Number(calculatedNum) / Number(displayNum));
+		default:
+			return;				
+	}
+}
+
+//Operation Keys
+document.getElementById('add').addEventListener('click', function(e) {
+	if (operation === null) {
+		calculatedNum = displayNum;
 	} else {
-		document.getElementById('display').innerHTML += num;
+		calculatedNum = calculate();	
 	}
-}
-
-function add(num1, num2) {
-	return num1 + num2;
-}
-
-function subtract(num1, num2) {
-	return num1 - num2;
-}
-
-function multiply(num1, num2) {
-	return num1 * num2;
-}
-
-function divide(num1, num2) {
-	return num1 / num2;
-}
-
-document.getElementById('clear').addEventListener('click', function(e) {
-	document.getElementById('display').innerHTML = '0';
+	displayNum = '0';
+	display(calculatedNum);
+	operation = 'add';
 });
 
-document.getElementById('memory-save').addEventListener('click', function(e) {
-	memoryRecall = document.getElementById('display').innerHTML;
-});
-
-document.getElementById('memory-clear').addEventListener('click', function(e) {
-	memoryRecall = null;
-});
-
-document.getElementById('memory-recall').addEventListener('click', function(e) {
-	if (memoryRecall != null) {
-		document.getElementById('display').innerHTML = memoryRecall;	
+document.getElementById('subtract').addEventListener('click', function(e) {
+	if (operation === null) {
+		calculatedNum = displayNum;
+	} else {
+		calculatedNum = calculate();	
 	}
+	displayNum = '0';
+	display(calculatedNum);
+	operation = 'subtract';
 });
 
-document.getElementById('seven').addEventListener('click', function(e) {
-	appendDigits('7');
+document.getElementById('multiply').addEventListener('click', function(e) {
+	if (operation === null) {
+		calculatedNum = displayNum;
+	} else {
+		calculatedNum = calculate();	
+	}
+	displayNum = '0';
+	display(calculatedNum);
+	operation = 'multiply';
 });
 
-document.getElementById('eight').addEventListener('click', function(e) {
-	appendDigits('8');
+document.getElementById('divide').addEventListener('click', function(e) {
+	if (operation === null) {
+		calculatedNum = displayNum;
+	} else {
+		calculatedNum = calculate();	
+		//need to account for divide by zero		
+	}
+	displayNum = '0';
+	display(calculatedNum);
+	operation = 'divide';
 });
 
-document.getElementById('nine').addEventListener('click', function(e) {
-	appendDigits('9');
+document.getElementById('equals').addEventListener('click', function(e) {
+	if (operation === null) {
+		calculatedNum = displayNum;
+	} else {
+		calculatedNum = calculate();	
+		//need to account for divide by zero		
+	}
+	displayNum = '0';
+	display(calculatedNum);
+	operation = null;
 });
 
-document.getElementById('divide').addEventListener('click', function(e) {});
-
-document.getElementById('four').addEventListener('click', function(e) {
-	appendDigits('4');
+//Number Keys
+document.getElementById('zero').addEventListener('click', function(e) {
+	updateDisplayNum('0');
+	display(displayNum);
 });
-
-document.getElementById('five').addEventListener('click', function(e) {
-	appendDigits('5');
-});
-
-document.getElementById('six').addEventListener('click', function(e) {
-	appendDigits('6');
-});
-
-document.getElementById('multiply').addEventListener('click', function(e) {});
 
 document.getElementById('one').addEventListener('click', function(e) {
-	appendDigits('1');
+	updateDisplayNum('1');	
+	display(displayNum);	
 });
 
 document.getElementById('two').addEventListener('click', function(e) {
-	appendDigits('2');
+	updateDisplayNum('2');	
+	display(displayNum);		
 });
 
 document.getElementById('three').addEventListener('click', function(e) {
-	appendDigits('3');
+	updateDisplayNum('3');
+	display(displayNum);		
 });
 
-document.getElementById('subtract').addEventListener('click', function(e) {});
+document.getElementById('four').addEventListener('click', function(e) {
+	updateDisplayNum('4');	
+	display(displayNum);		
+});
+
+document.getElementById('five').addEventListener('click', function(e) {
+	updateDisplayNum('5');	
+	display(displayNum);		
+});
+
+document.getElementById('six').addEventListener('click', function(e) {
+	updateDisplayNum('6');	
+	display(displayNum);		
+});
+
+document.getElementById('seven').addEventListener('click', function(e) {
+	updateDisplayNum('7');
+	display(displayNum);		
+});
+
+document.getElementById('eight').addEventListener('click', function(e) {
+	updateDisplayNum('8');
+	display(displayNum);		
+});
+
+document.getElementById('nine').addEventListener('click', function(e) {
+	updateDisplayNum('9');		
+	display(displayNum);	
+});
 
 document.getElementById('decimal').addEventListener('click', function(e) {
 	if (noDecimal()) {
-		if (notZero()) {
-			appendDigits('.');
-		} else {
-			document.getElementById('display').innerHTML = '0.';
-		}
+		updateDisplayNum('.');
+	}	
+	display(displayNum);	
+});
+
+//Other Keys
+document.getElementById('clear').addEventListener('click', function(e) {
+	operation = null;
+	calculatedNum = null;
+	displayNum = '0';
+	display(displayNum);
+});
+
+document.getElementById('memory-save').addEventListener('click', function(e) {
+	memory = displayNum;
+});
+
+document.getElementById('memory-clear').addEventListener('click', function(e) {
+	memory = null;
+});
+
+document.getElementById('memory-recall').addEventListener('click', function(e) {
+	if (memory != null) {
+		displayNum = memory;
+		display(displayNum);
 	}
 });
-
-document.getElementById('zero').addEventListener('click', function(e) {
-	appendDigits('0');
-});
-
-document.getElementById('equals').addEventListener('click', function(e) {});
-
-document.getElementById('add').addEventListener('click', function(e) {});
